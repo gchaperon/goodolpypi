b3aea09b38d38c3e4ecc62a3ae49e308f7c453b6
gchaperon <gchaperon@protonmail.com>
2020-11-06 18:17:03 -0300

    added main logic, add specifier (based on date) to requirements that dont have one already

--- goodolpypi/main.py
+++ goodolpypi/main.py
@@ -1,39 +1,57 @@
 from urllib.request import urlopen
+from functools import partial
+import re
 import json
 import argparse
 from datetime import datetime, timezone
 from itertools import zip_longest
 from operator import itemgetter
-from packaging import version
+from packaging.version import parse as parse_version, Version, LegacyVersion
+from packaging.requirements import Requirement
+from typing import Dict, Tuple, cast, List, Union, Protocol
+from concurrent.futures import ThreadPoolExecutor
+
+ParsedVersion = Union[Version, LegacyVersion]
 
 
-def fetch_releases(project):
+def fetch_releases(project: str) -> Dict[ParsedVersion, list]:
     pypi_endpoint = f"https://pypi.org/pypi/{project}/json"
-    response = urlopen(pypi_endpoint)
-    releases = json.loads(response.read().decode()).get("releases")
-    return releases
+    with urlopen(pypi_endpoint) as response:
+        releases: Dict[str, list] = json.loads(response.read().decode()).get(
+            "releases"
+        )
+    return {
+        parse_version(version): files for version, files in releases.items()
+    }
 
 
-def parse_date(date_str):
-    # defaults for year, month, day
-    return datetime(
-        *[
+def parse_date(date_str: str) -> datetime:
+    assert re.match(r"^\d{4}(-\d{2}){0,2}", date_str), "Invalid string format"
+
+    dt_args = cast(
+        Tuple[int, int, int],
+        [
             selected or default
             for selected, default in zip_longest(
                 map(int, date_str.split("-")), [1, 1, 1], fillvalue=None
             )
         ],
+    )
+    return datetime(
+        *dt_args,
         tzinfo=timezone.utc,
     )
 
 
-def fetch_latest_version(date, project):
-    releases = fetch_releases(project)
+def fetch_latest_version(
+    date: datetime, requirement: Requirement
+) -> ParsedVersion:
+    releases = fetch_releases(requirement.name)
     # releases with all files uploaded before target date
     all_valid = [
         version
         for version, files in releases.items()
-        if files
+        if files  # apparently, some releases don't have files
         and all(
             datetime.fromisoformat(
                 file["upload_time_iso_8601"].replace("Z", "+00:00")
@@ -42,19 +60,43 @@ def fetch_latest_version(date, project):
             for file in files
         )
     ]
-    *_, latest = sorted(all_valid, key=version.parse)
+    *_, latest = sorted(all_valid)
     return latest
 
 
-def main():
+def process_requirement(
+    date: datetime, requirement: Requirement
+) -> Requirement:
+    # add specifier only if the req doesn't have one
+    if not requirement.specifier:
+        latest_version = fetch_latest_version(date, requirement)
+        requirement.specifier &= f"<={latest_version}"
+    return requirement
+
+
+class Arguments(Protocol):
+    date: datetime
+    requirements: List[Requirement]
+
+
+def main() -> None:
     parser = argparse.ArgumentParser(
         description="Fetch latest version of pypi packages at a given date"
     )
     parser.add_argument("date", type=parse_date)
-    parser.add_argument("packages", metavar="package", nargs="+")
-    args = parser.parse_args()
-    for package in args.packages:
-        print(fetch_latest_version(args.date, package))
+    parser.add_argument(
+        "requirements", metavar="pkg", nargs="+", type=Requirement
+    )
+
+    args: Arguments = parser.parse_args()
+
+    #    for requirement in args.requirements:
+    #       print(process_requirement(args.date, requirement))
+    with ThreadPoolExecutor(max_workers=10) as e:
+        for requirement in e.map(
+            partial(process_requirement, args.date), args.requirements
+        ):
+            print(requirement)
 
 
 if __name__ == "__main__":
